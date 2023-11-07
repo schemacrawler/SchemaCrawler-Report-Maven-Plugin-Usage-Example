70c16949ded8b9366e5eb7f71be9360981f6062f
Sualeh Fatehi <sualeh@hotmail.com>
2023-11-07 18:38:17 -0500

    v16.20.5

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,10 @@
 package com.example;
 
 
-public class App
-{
-  
-  public static void main(String[] args)
-  {
+public class App {
+
+  public static void main(String[] args) {
     System.out.println("Hello World!");
   }
-  
+
 }
